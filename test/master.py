d51669a4e53ef1f5ac9267e7fc8dead72b8d34aa
Shamsul Alam <jccbuet1@gmail.com>
2022-08-06 23:32:35 +0600

    a

---
+++ test/master.py
@@ -0,0 +1,12 @@
+"""
+Initialization:
+1. Load Configs: Bay name, SS Name, Alarm limits etc in variable / array
+2. Load System : Get system param settings like ct, pt ratio etc and store in variable / array
+
+Thread 1 :
+    Loop: read values from Modbus, store in a DF (10 devices, 1/2 time as appropriate); Async Push values to DB
+            Avoid bottleneck
+
+Thread 2 :
+    Loop: Get values from DF (not DB) and update GUI (refresh rate 300ms)
+"""
